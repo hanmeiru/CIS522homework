129db7f76849b38735f96a0a1b0655f6bffbd6e9
hanmeiru <103174944+hanmeiru@users.noreply.github.com>
2023-02-05 23:13:36 -0500

    Add files via upload

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,15 +1,16 @@
 import torch
+import torch.nn as nn
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
         num_classes: int,
         hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
+        activation: Callable = torch.nn.ReLU(),
         initializer: Callable = torch.nn.init.ones_,
     ) -> None:
         """
@@ -22,7 +23,28 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()  # inheriting
+
+        # defining layers
+        self.layers = nn.ModuleList()
+        self.activation = activation
+        self.layers += [
+            nn.Linear(input_size, hidden_size),
+            self.activation,
+        ]  # first layer
+        hidden_idx = 0  # idx of hidden layers
+        while hidden_idx < hidden_count - 1:
+            self.layers += [
+                nn.Linear(hidden_size, hidden_size),
+                self.activation,
+            ]  # hidden layers
+            hidden_idx += 1
+        self.layers += [nn.Linear(hidden_size, num_classes)]  # last layer
+
+        # initialize using specified initializer
+        for m in self.layers:
+            if type(m) == nn.Linear:
+                initializer(m.weight)
 
     def forward(self, x):
         """
@@ -34,4 +56,12 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+
+        # Flatten inputs to 2D (if more than that)
+        x = torch.flatten(x, start_dim=1)
+
+        # Get activations of each layer
+        for layer in self.layers:
+            x = layer(x)
+
+        return x

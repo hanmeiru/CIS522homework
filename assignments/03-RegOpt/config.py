560135e2b7d9acf4b383d960996dad3f5088ac45
hanmeiru <103174944+hanmeiru@users.noreply.github.com>
2023-02-21 00:10:12 -0500

    Add files via upload

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -10,7 +10,7 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 9
+    num_epochs = 10
     initial_learning_rate = 0.003
     initial_weight_decay = 0
 

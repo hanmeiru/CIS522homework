6f2ee7f7e06e24639d32c79a8df87b6fbae83665
hanmeiru <103174944+hanmeiru@users.noreply.github.com>
2023-03-16 02:02:39 -0400

    Add files via upload

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -17,21 +17,21 @@ class Model(torch.nn.Module):
         Initializes the layers of the neural net
         """
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 8, 3)
-        self.conv2 = nn.Conv2d(8, 32, 3)
+        self.conv1 = nn.Conv2d(num_channels, 16, 3)
+        # self.conv2 = nn.Conv2d(8, 32, 3)
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(
-            196 * 32, 64
-        )  # (32-4*2)/2 for width and height, 32 filters
-        self.fc2 = nn.Linear(64, num_classes)
+            15*15*16, 128
+        )  # (32-2)/2 for width and height, 32 filters
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Passing the input image to the model and get output
         """
         x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
+        # x = self.relu(self.conv2(x))
         x = self.maxpool(x)
         x = torch.flatten(x, 1)  # (8,2304)
         x = self.relu(self.fc1(x))
